9d6df2c44c0c58b26f07aa6b1376c137e79ba704
rznmgr630 <rajan.midun1@gmail.com>
2024-08-06 13:39:47 +0545

    ROUTING: basic with dynamic routing

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template
-app = Flask(__name__)
+from flask import Flask,render_template;
 
+app=Flask(__name__)
+
+# Note: The route /flask can only handle the url /path but /python/ can handle both  /python and /python/
 @app.route('/')
 def hello_world():
+   return 'Hello world'
+
+@app.route('/<int:userId>')
+def get_user(userId):
+   # return f'User id is {userId}'
+   return 'User id is %d' % userId
+
+@app.route('/html')
+def get_html():
    return render_template('index.html')
 
-if __name__ == '__main__':
-   app.debug=True
-   app.run()
+if __name__=='__main__':
+   app.run(debug=True)
