815f5ebeede37d063706eda46c8e0fe1e69c36dd
rznmgr630 <rajan.midun1@gmail.com>
2024-08-06 16:30:42 +0545

    sqlalchemy: setup

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask,redirect,url_for,request,render_template;
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__);
 
-@app.route('/')
-def home_page():
-   return render_template('index.html')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 
 if __name__=='__main__':
    app.run(debug=True)
