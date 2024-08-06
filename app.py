41aa3635ee87685a99fe186605e2e8ee0233702f
rznmgr630 <rajan.midun1@gmail.com>
2024-08-06 14:34:05 +0545

    static_files: serving static files

--- app.py
+++ app.py
@@ -2,32 +2,9 @@ from flask import Flask,redirect,url_for,request,render_template;
 
 app=Flask(__name__);
 
-# The jinja2 template engine uses the following delimiters for escaping from HTML.
-# {% ... %} for Statements
-# {{ ... }} for Expressions to print to the template output
-# {# ... #} for Comments not included in the template output
-# ... ## for Line Statements
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-   if request.method=='POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      return render_template('login.html')
-   
-@app.route('/success/<name>')
-def success(name):
-   return render_template('success.html',name=name)
-
-@app.route('/result')
-def result():
-   dict={
-      'math':34,
-      'science':24,
-      'english':56
-   }
-   return render_template('result.html',score=dict)
+@app.route('/')
+def home_page():
+   return render_template('index.html')
 
 if __name__=='__main__':
    app.run(debug=True)
